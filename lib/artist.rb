99ef3e9a2319b4de03d006a30f9fc75bbf6f64d7
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-15 01:19:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,6 +15,9 @@ class Artist
     song.artist = self 
   end 
   
-  def add_song_by_name
+  def add_song_by_name(name)
     song=song.new(name)
-    
+    song.artist=self
+  end
+  
+  def .song_count(count)
